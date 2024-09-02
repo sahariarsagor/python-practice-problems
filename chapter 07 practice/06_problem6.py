2aff99afa0bd2603466ff96c2b01b806d16a34b9
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-02 15:37:48 +0600

    add problem 06

---
+++ chapter 07 practice/06_problem6.py
@@ -0,0 +1,10 @@
+'''
+Leap Year Cracker
+'''
+
+year = int(input("Enter Year: "));
+
+if((year%4 == 0 and year%100 != 0) or (year%400 == 0)):
+    print(f"{year} is a leap year");
+else:
+    print(f"{year} is not a leap year");
