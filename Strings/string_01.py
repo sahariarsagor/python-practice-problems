c4e4a735c6daf84932210e99af0857631af4e719
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-19 01:03:44 +0600

    string

---
+++ Strings/string_01.py
@@ -0,0 +1,13 @@
+'''
+Note: 
+String is immutable which mean if you create a string you cannot change it. 
+'''
+#declaring string 
+# string_name = "your string here"; 
+# print(string_name);
+
+myName = "Sahariar Sagor"; 
+print("my name is: ", myName);
+
+#to print a length of a string: 
+print(len(myName));
