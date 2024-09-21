66ccab80d11d4ea4ea24df595c0f65f3e1c85bbf
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-21 22:50:05 +0600

    list method

---
+++ List/list_method_02.py
@@ -0,0 +1,30 @@
+# sort(): Sorts the list in ascending order (in-place, meaning it modifies the list).
+
+# myList = [2, 4, 6, 8];
+# myList.sort();
+# print(myList);
+
+# reverse(): Reverses the order of the list (in-place).
+
+# myList = [2, 8, 5, 6];
+# myList.reverse();
+# print(myList);
+
+# extend(iterable): Extends the list by appending all elements from another iterable (like another list).
+
+# myList = [1, 2, 3];
+# myList.extend([4, 5, 6]);
+# print(myList);
+
+# count(item): Returns the number of times an item appears in the list.
+
+# myList = [2, 4, 6, 2, 4, 5, 2];
+# returnValue = myList.count(2);
+# print(myList);
+# print(returnValue);
+
+#clear(): Removes all items from the list.
+
+# myList = [2, 4, 6];
+# myList.clear();
+# print(myList);
