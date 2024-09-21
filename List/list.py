86e81676f44641b3c16b45040f7f7d6e860d66c0
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-21 21:59:33 +0600

    list

---
+++ List/list.py
@@ -0,0 +1,9 @@
+'''
+    A list in Python is a collection of items (elements) that can be of different data types. Lists are ordered, mutable (you can change the values), and allow duplicate values. Lists are created using square brackets ([]).
+'''
+
+#Creating a list 
+s = ["sahariar", 5, 10, 20, True, "Sorna"];
+
+# accessing a list using index number. 
+print(s[3]);
