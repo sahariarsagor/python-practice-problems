d07be08050de3939ccefb4f626a4c4a66a682e32
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-21 22:40:48 +0600

    list method

---
+++ List/list_method_01.py
@@ -0,0 +1,21 @@
+#append(item): add an item to the end of the list. 
+
+myList = [2, 4, 6];
+# myList.append(8);
+
+# insert(index, item): insert a item at a specific position in the list. 
+# myList.insert(1, "a"); [2, 'a', 4, 6]
+
+#remove(item): Removes the first occurrence of the item from the list.
+# myList.remove(2);
+
+#pop(index): Removes and returns the item at the specified index. If no index is provided, it removes the last item.
+
+# returnValue = myList.pop(2);
+
+#index(item): Returns the index of the first occurrence of the item.
+
+# returnValue = myList.index(4);
+
+print(myList); 
+# print(returnValue);
